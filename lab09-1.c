e4238886804d9892db3c8df2443aacfb2d78da38
justin-h-m-tunis <36137881+justin-h-m-tunis@users.noreply.github.com>
2018-04-23 19:06:05 -0400

    Update lab09-1.c

--- lab09-1.c
+++ lab09-1.c
@@ -55,40 +55,41 @@ int path[LENGTH][2] = {{13,146},
 
 
 
-void goToTheta(int t1, int t2){
-	while(nMotorEncoder(J1)!=t1-J1_INIT||nMotorEncoder(J2)!=t2-J2_INIT){
+void goToTheta(int t1, int t2){ //puts joints at desired theta one and theta two
+	
+	while(nMotorEncoder(J1)!=t1-J1_INIT||nMotorEncoder(J2)!=t2-J2_INIT){ //while both joints are not at the desired location
 		
 		nxtDisplayClearTextLine(5);
 		nxtDisplayClearTextLine(6);
 		nxtDisplayString(5,"%d",nMotorEncoder(J1));
 		nxtDisplayString(6,"%d",nMotorEncoder(J2));
 		
-		if(nMotorEncoder(J1) < t1-J1_INIT)
-				motor[J1] = 10;
-		else if(nMotorEncoder(J1) > t1-J1_INIT)
-				motor[J1] = -10;
-		else
-				motor[J1] = 0;
+		if(nMotorEncoder(J1) < t1-J1_INIT) //if motor encodes is less than desired 
+				motor[J1] = 10; //go forewards
+		else if(nMotorEncoder(J1) > t1-J1_INIT) //if greater
+				motor[J1] = -10; //go backwards
+		else				//if equal
+				motor[J1] = 0; //stop
 		
-		if(nMotorEncoder(J2) < t2-J2_INIT)
+		if(nMotorEncoder(J2) < t2-J2_INIT) //same thing but for the other motor
 				motor[J2] = 10;
 		else if(nMotorEncoder(J2) > t2-J2_INIT)
 				motor[J2] = -10;
 		else
 				motor[J2] = 0;
 	}
-	motor[J1] = 0;
+	motor[J1] = 0; //stops both motors
 	motor[J2] = 0;
 }
 
 task main()
 {
-	nMotorEncoder(J1) = 0;
+	nMotorEncoder(J1) = 0; //reset encoders
 	nMotorEncoder(J2) = 0;
 	for(int i = 0; i< LENGTH; i++){
-		goToTheta((int)path[i][0], (int)path[i][1]);
+		goToTheta((int)path[i][0], (int)path[i][1]); //for each angle pair, go to said angles
 	}
-	while(1){
+	while(1){ //just displays encoder values
 		nxtDisplayClearTextLine(5);
 		nxtDisplayClearTextLine(6);
 		nxtDisplayString(5,"%d",nMotorEncoder(J1));
