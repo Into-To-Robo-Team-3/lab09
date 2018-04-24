9d1b0d4cbe923f22befb382572eaadf22d7b8116
Obi Adubor <oadubor@gmail.com>
2018-04-23 20:26:03 -0700

    Update lab09-1.c
    Fixed backlash scaling bug

--- lab09-1.c
+++ lab09-1.c
@@ -4,15 +4,15 @@
 
 //#define LENGTH 41
 #define LENGTH 1
+#define TF 1
 #define J1_INIT 0
 #define J2_INIT 180
 
 #define J1_SCALE 0.94 //done tuning
-#define J2_SCALE 5 	//needs to be tuned 
+#define J2_SCALE 3.75 	//needs to be tuned 
 
 int path[LENGTH][2] = {
-{-180, 180}
-};
+{0,90}};
 
 
 
@@ -20,7 +20,7 @@ void goToTheta(int t1, int t2){ //puts joints at desired theta one and theta two
 	
 	int tick = 0; //encoder ticks, used for joint 2 offset
 	
-	while(nMotorEncoder(J1)!= round(J1_SCALE*(t1-J1_INIT))){ //||nMotorEncoder(J2)!=J2_SCALE*(t2-J2_INIT)){ //while both joints are not at the desired location
+	while(nMotorEncoder(J1)!= round(J1_SCALE*(t1-J1_INIT)) || nMotorEncoder(J2)!= round(J2_SCALE*(t2-J2_INIT)) + round(J1_SCALE*(t1-J1_INIT)/TF)){ //while both joints are not at the desired location
 		//nxtDisplayString(4,"%d",tick);
 		
 		nxtDisplayClearTextLine(5);
@@ -32,30 +32,35 @@ void goToTheta(int t1, int t2){ //puts joints at desired theta one and theta two
 		
 		if(nMotorEncoder(J1) < round(J1_SCALE*(t1-J1_INIT))){ //if motor encodes is less than desired 
 				motor[J1] = 10; //go forewards
-				if(tick % 4 == 0){ //add offset to second joint to offset backlash
-					t2 = t2 + 1;
-				}
+				//if(tick % 4 == 0){ //add offset to second joint to offset backlash
+					//t2 = t2 + 1;
+				//}
 		}			
 		else if(nMotorEncoder(J1) > round(J1_SCALE*(t1-J1_INIT))){ //if greater
 				motor[J1] = -10; //go backwards
-				if(tick % 4 == 0){ //add offset to second joint to offset backlash
-					t2 = t2 - 1;
-				}	
+				//if(tick % 4 == 0){ //add offset to second joint to offset backlash
+					//t2 = t2 - 1;	
+				//}	
 		}		
-		else				//if equal
+		else{				//if equal
 				motor[J1] = 0; //stop
-		
+		}
 		//nxtDisplayString(7,"%d",t2);
-		if(nMotorEncoder(J2) < round(J2_SCALE*(t2-J2_INIT))) //same thing but for the other motor
+		if(nMotorEncoder(J2) < round(J2_SCALE*(t2-J2_INIT)) + round(J1_SCALE*(t1-J1_INIT)/TF)){ //same thing but for the other motor
 				motor[J2] = 10;
-		else if(nMotorEncoder(J2) > round(J2_SCALE*(t2-J2_INIT)))
+				nxtDisplayString(7,"1");
+		}
+		else if(nMotorEncoder(J2) > round(J2_SCALE*(t2-J2_INIT)) + round(J1_SCALE*(t1-J1_INIT)/TF)){
 				motor[J2] = -10;
-		else
+				nxtDisplayString(7,"2");
+		}
+		else{
+				nxtDisplayString(7,"3");
 				motor[J2] = 0;
+		}
 	}
 	motor[J1] = 0; //stops both motors
 	motor[J2] = 0;
-	
 }
 
 task main()
